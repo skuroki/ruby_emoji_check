35983e5c1bed2134d2250fc8860791f8f99ce956
Shinsuke KUROKI <shinsuke.kuroki@finc.com>
2021-11-08 21:53:02 +0900

    Initial commit

---
+++ main.rb
@@ -0,0 +1,33 @@
+PATTERN = /[(\p{Emoji}\p{Emoji_Component})&&[:^ascii:]]/
+
+STRINGS_WITH_EMOJI = [
+  '🍣',
+  '0️⃣',
+  "👩‍👩‍👧‍👧", "☃️", "🇵","🏻", "😴", "▶️", "🛌🏽", "🇵🇹", "🏴󠁧󠁢󠁳󠁣󠁴󠁿", "2️⃣", "🤾🏽‍♀️",
+].freeze
+
+STRINGS_WITHOUT_EMOJI = [
+  '1aあア＠',
+  '東京都江東区新木場２丁目２−１０',
+  '黒木　慎介',
+].freeze
+
+result = true
+
+STRINGS_WITH_EMOJI.each do |s|
+  unless s.match?(PATTERN)
+    puts s + 'は絵文字を含むと判定されませんでした'
+    result = false
+  end
+end
+
+STRINGS_WITHOUT_EMOJI.each do |s|
+  if s.match?(PATTERN)
+    puts s + 'は絵文字を含むと判定されてしまいました'
+    result = false
+  end
+end
+
+if result
+  puts '判定は成功しました'
+end
